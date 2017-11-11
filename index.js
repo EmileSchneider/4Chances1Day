fd3c52cba38c428d8fc59580f34dc02baad1d213
EmileSchneider <emileschneider7@gmail.com>
2017-11-11 19:07:27 +0100

    commit 6

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 
 
 https://github.com/EmileSchneider/4Chances1Day.git
@@ -15,8 +16,14 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.post('/', function(req, res) {
-  console.log("helo wer")
+  console.log(req.body.user.name);
+  console.log(req.body.user.email);
 });
 
 app.listen(app.get('port'), function() {
